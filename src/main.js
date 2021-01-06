2bb020169743904a6ab808ab186c4ccc5ee4045a
Steven Rollason <2099542+gadgetchnnel@users.noreply.github.com>
2021-01-06 19:24:57 +0000

    Updated to FullCalendar 5 and styling changes

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import coreCSS from '@fullcalendar/core/main.css';
+import coreCSS from '@fullcalendar/common/main.css';
 import daygridCSS from '@fullcalendar/daygrid/main.css';
-import timegridCSS from '@fullcalendar/timegrid/main.css';
 import tippy from 'tippy.js';
 import tippyCSS from 'tippy.js/dist/tippy.css';
 import tippyAnimationCSS from 'tippy.js/animations/scale-extreme.css';
@@ -46,19 +45,252 @@ class FullCalendarCard extends LitElement {
 		return html`<style type="text/css">
       		${coreCSS}
 			${daygridCSS}
-			${timegridCSS}
 			${tippyCSS}
 			${tippyAnimationCSS}
 			${tippyThemeCSS}
 			
-			.fc-unthemed .fc-today {
-  				background: rgba(var(--rgb-primary-text-color), 0.5) !important;
-  				opacity: 0.5 !important;
-  				color: black !important;
-    			border: none !important;
-    			border-top: 1px solid #ddd !important;
-    			font-weight: bold;
-			}
+			:host {
+          display: flex;
+          flex-direction: column;
+          --fc-theme-standard-border-color: var(--divider-color);
+        }
+
+        .header {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          padding-bottom: 8px;
+        }
+
+        :host([narrow]) .header {
+          padding-right: 8px;
+          padding-left: 8px;
+          flex-direction: column;
+          align-items: flex-start;
+          justify-content: initial;
+        }
+
+        .navigation {
+          display: flex;
+          align-items: center;
+          flex-grow: 0;
+        }
+
+        a {
+          color: var(--primary-text-color);
+        }
+
+        .controls {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 100%;
+        }
+
+        .today {
+          margin-right: 20px;
+        }
+
+        .prev,
+        .next {
+          --mdc-icon-button-size: 32px;
+        }
+
+        ha-button-toggle-group {
+          color: var(--primary-color);
+        }
+
+        #calendar {
+          flex-grow: 1;
+          background-color: var(
+            --ha-card-background,
+            var(--card-background-color, white)
+          );
+          min-height: 400px;
+          --fc-neutral-bg-color: var(
+            --ha-card-background,
+            var(--card-background-color, white)
+          );
+          --fc-list-event-hover-bg-color: var(
+            --ha-card-background,
+            var(--card-background-color, white)
+          );
+          --fc-theme-standard-border-color: var(--divider-color);
+          --fc-border-color: var(--divider-color);
+        }
+
+        a {
+            color: inherit !important; 
+        }
+
+        .fc-theme-standard .fc-scrollgrid {
+          border: 1px solid var(--divider-color);
+        }
+
+        .fc-scrollgrid-section-header td {
+          border: none;
+        }
+
+        th.fc-col-header-cell.fc-day {
+          color: var(--secondary-text-color);
+          font-size: 11px;
+          font-weight: 400;
+          text-transform: uppercase;
+        }
+
+        .fc-daygrid-dot-event:hover {
+          background-color: inherit
+        }
+
+        .fc-daygrid-day-top {
+          text-align: center;
+          padding-top: 5px;
+          justify-content: center;
+        }
+
+        table.fc-scrollgrid-sync-table
+          tbody
+          tr:first-child
+          .fc-daygrid-day-top {
+          padding-top: 0;
+        }
+
+        a.fc-daygrid-day-number {
+          float: none !important;
+          font-size: 12px;
+        }
+
+        .fc .fc-daygrid-day-number {
+            padding: 3px !important;
+        }
+
+        .fc .fc-daygrid-day.fc-day-today {
+          background: inherit;
+        }
+
+        td.fc-day-today .fc-daygrid-day-top {
+          padding-top: 4px;
+        }
+
+        td.fc-day-today .fc-daygrid-day-number {
+          height: 24px;
+          color: var(--text-primary-color) !important;
+          background-color: var(--primary-color);
+          border-radius: 50%;
+          display: inline-block;
+          text-align: center;
+          white-space: nowrap;
+          width: max-content;
+          min-width: 24px;
+          line-height: 140%;
+        }
+
+        .fc-daygrid-day-events {
+          margin-top: 4px;
+        }
+
+        .fc-event {
+          border-radius: 4px;
+          line-height: 1.7;
+        }
+
+        .fc-daygrid-block-event .fc-event-main {
+          padding: 0 1px;
+        }
+
+        .fc-day-past .fc-daygrid-day-events {
+          opacity: 0.5;
+        }
+
+        .fc-icon-x:before {
+          font-family: var(--material-font-family);
+          content: "X";
+        }
+
+        .fc-popover {
+          background-color: var(--primary-background-color) !important;
+        }
+
+        .fc-popover-header {
+          background-color: var(--secondary-background-color) !important;
+        }
+
+        .fc-theme-standard .fc-list-day-frame {
+          background-color: transparent;
+        }
+
+        .fc-list.fc-view,
+        .fc-list-event.fc-event td {
+          border: none;
+        }
+
+        .fc-list-day.fc-day th {
+          border-bottom: none;
+          border-top: 1px solid var(--fc-theme-standard-border-color, #ddd) !important;
+        }
+
+        .fc-list-day-text {
+          font-size: 16px;
+          font-weight: 400;
+        }
+
+        .fc-list-day-side-text {
+          font-weight: 400;
+          font-size: 16px;
+          color: var(--primary-color);
+        }
+
+        .fc-list-table td,
+        .fc-list-day-frame {
+          padding-top: 12px;
+          padding-bottom: 12px;
+        }
+
+        :host([narrow]) .fc-dayGridMonth-view
+          .fc-daygrid-dot-event
+          .fc-event-time,
+        :host([narrow]) .fc-dayGridMonth-view
+          .fc-daygrid-dot-event
+          .fc-event-title,
+          :host([narrow]) .fc-dayGridMonth-view .fc-daygrid-day-bottom {
+          display: none;
+        }
+
+        :host([narrow]) .fc .fc-dayGridMonth-view .fc-daygrid-event-harness-abs {
+          visibility: visible !important;
+          position: static;
+        }
+
+        :host([narrow]) .fc-dayGridMonth-view .fc-daygrid-day-events {
+          display: flex;
+          min-height: 2em !important;
+          justify-content: center;
+          flex-wrap: wrap;
+          max-height: 2em;
+          height: 2em;
+          overflow: hidden;
+        }
+
+        :host([narrow]) .fc-dayGridMonth-view .fc-scrollgrid-sync-table {
+          overflow: hidden;
+        }
+
+        .fc-scroller::-webkit-scrollbar {
+          width: 0.4rem;
+          height: 0.4rem;
+        }
+
+        .fc-scroller::-webkit-scrollbar-thumb {
+          -webkit-border-radius: 4px;
+          border-radius: 4px;
+          background: var(--scrollbar-thumb-color);
+        }
+
+        .fc-scroller {
+          overflow-y: auto;
+          scrollbar-color: var(--scrollbar-thumb-color) transparent;
+          scrollbar-width: thin;
+        }
       		</style>`;
 	}
 	
@@ -151,10 +383,11 @@ class FullCalendarCard extends LitElement {
 		this.calendar = new Calendar(calendarEl, {
     		plugins: [ dayGridPlugin, timeGridPlugin, interactionPlugin ],
     		aspectRatio: this._isPanel ?  (portrait ? 0.4 : 2.5) : 1.35,
+    		eventDisplay: "auto",
     		header: {
-  				left:   'title',
-  				center: '',
-  				right:  'today dayGridMonth timeGridWeek timeGridDay prev,next'
+  				start:   'today prev, next',
+  				center: 'title',
+  				right:  'dayGridMonth dayGridWeek dayGridDay'
 			},
     		eventTimeFormat: {
     			hour: 'numeric',
@@ -165,7 +398,7 @@ class FullCalendarCard extends LitElement {
   			windowResize: () => {
     			this.updateAspectRatio();
   			},
-  			eventRender: function(info) {
+  			eventDidMount: function(info) {
     			tippy(info.el, {
   					content: `<div style="min-width:40em; min-height:10em;">
   						<h2>${info.event.title}</h2>
